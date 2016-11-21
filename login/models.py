528019cc89d0f2d8d65a9f4530b2f1e226205600
Jersyliu <jersyliu@outlook.com>
2016-11-20 23:30:29 -0500

    working

--- login/models.py
+++ login/models.py
@@ -23,8 +23,8 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     lesson_name = models.CharField(max_length=200)
     content = models.CharField(max_length=200)
-    summary = models.TextField(default=None)
-    expected_output = models.CharField(max_length=200, default=None)
+    summary = models.TextField(default="")
+    expected_output = models.CharField(max_length=200, default="")
     point_value = models.IntegerField(default=10)
     #video
     def __str__(self):
@@ -34,8 +34,8 @@ class Progress(models.Model):
     newuser = models.ForeignKey(NewUser, on_delete=models.CASCADE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     is_complete = models.BooleanField(default=False)
-    progress_until_now = models.TextField(default=None)
-    notes = models.TextField( default=None)
+    progress_until_now = models.TextField(default="")
+    notes = models.TextField( default="")
     
     def __str__(self):
         return  str(self.id) + ";" + self.newuser.username+";"+self.progress_until_now
@@ -44,7 +44,7 @@ class Challenge(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     challenge_name = models.CharField(max_length=200)
     content = models.CharField(max_length=200)
-    expected_output = models.CharField(max_length=200, default=None)
+    expected_output = models.CharField(max_length=200, default="")
     point_value = models.IntegerField(default=20)
     
     def __str__(self):
@@ -54,8 +54,8 @@ class ChallengeProgress(models.Model):
     newuser = models.ForeignKey(NewUser, on_delete=models.CASCADE)
     challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
     is_complete = models.BooleanField(default=False)
-    progress_until_now = models.TextField(default=None)
-    notes = models.TextField(default=None)
+    progress_until_now = models.TextField(default="")
+    notes = models.TextField(default="")
     
     def __str__(self):
         return self.newuser.username+";"+self.progress_until_now
